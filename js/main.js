4d0c6f17e20894df456b321d50dd1b452684c907
Shusev <nikolayshusev@mail.ru>
2016-02-20 00:30:55 +0300

    таблица цен

--- js/main.js
+++ js/main.js
@@ -18,4 +18,29 @@ $(document).ready(function(){
         }
     );
 
+    //hover строк таблицы
+
+    //hover_tr($(".tr1"), $(".td1"));
+    hover_tr($(".tr2"), $(".td2"));
+    hover_tr($(".tr3"), $(".td3"));
+    hover_tr($(".tr4"), $(".td4"));
+    hover_tr($(".tr5"), $(".td5"));
+    hover_tr($(".tr6"), $(".td6"));
+    hover_tr($(".tr7"), $(".td7"));
+    hover_tr($(".tr8"), $(".td8"));
+    hover_tr($(".tr9"), $(".td9"));
+
+    function hover_tr(a, b) {
+        a.hover(
+            function(){
+                a.css("background-color","#f0cdc5");
+                b.css("background-color","#f0cdc5");
+            },
+            function(){
+                a.css("background-color","white");
+                b.css("background-color","white");
+            }
+        );
+    }
+
 });
