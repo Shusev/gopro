00a4e7a9a927724733ad0aaa6d0bb6232ba409a8
Shusev <nikolayshusev@mail.ru>
2016-04-16 18:07:55 +0300

    80%

--- js/main.js
+++ js/main.js
@@ -111,53 +111,155 @@ $(document).ready(function(){
 
     //carousel
     var mySwiper = new Swiper ('.swiper-container', {
-        slidesPerView: 4,
-        freeMode: true,
+        slidesPerView: "auto",
+        //freeMode: true,
+        autoplay: 2000,
+        autoplayDisableOnInteraction: false,
         loop: true,
-        centeredSlides: true,
-        breakpoints: {
-            1024: {
-                slidesPerView: 4,
-                spaceBetween: 40
-            },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 30
-            },
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 20
-            },
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            }
+        centeredSlides: true
+        //breakpoints: {
+        //    1024: {
+        //        slidesPerView: 4,
+        //        spaceBetween: 40
+        //    },
+        //    768: {
+        //        slidesPerView: 3,
+        //        spaceBetween: 30
+        //    },
+        //    640: {
+        //        slidesPerView: 2,
+        //        spaceBetween: 20
+        //    },
+        //    320: {
+        //        slidesPerView: 1,
+        //        spaceBetween: 10
+        //    }
+        //}
+    });
+
+
+    //animation
+
+    $(window).scroll(function(){
+        var a = $(window).scrollTop();
+        if ((a >= 2795) && (a <= 2805)) {
+            var currentNumber, t;
+
+            currentNumber = $(".range").val();
+
+            $({numberValue: currentNumber}).animate({numberValue: 25}, {
+                duration: 2000,
+                easing: 'linear',
+                step: function() {
+                    $(".range").val(Math.ceil(this.numberValue));
+                    t = $(".range").val();
+                }
+            });
+            setTimeout(function() {
+                $({numberValue: t}).animate({numberValue: 3}, {
+                    duration: 500,
+                    easing: 'linear',
+                    step: function() {
+                        $(".range").val(Math.ceil(this.numberValue));
+                    }
+                });
+            }, 2000);
         }
     });
+
+
+    //sum calculate
 
     setInterval(function() {
-        $(".qual").val($(".range").val());
+        var summa = $("#summa"),
+            skidka = $("#skidka"),
+            days = $("#days"),
+            sum_per_day = $("#sum_per_day"),
+            range = $("#range").val(),
+
+
+            kit = $("#kit").val(),
+            head = $("#head").val(),
+            grud = $("#grud").val(),
+            monopod = $("#monopod").val(),
+            poplavok = $("#poplavok").val(),
+            truba = $("#truba").val(),
+            noga = $("#noga").val(),
+            shlem = $("#shlem").val(),
+            kayt = $("#kayt").val(),
+            foto = $("#foto").val(),
+            kepka = $("#kepka").val(),
+            car = $("#car").val(),
+            pult = $("#pult").val(),
+            akk = $("#akk").val(),
+            sd = $("#sd").val(),
+
+            sum_0,
+            sum_15,
+            sum_30,
+            sum_50;
+
+        sum_0 = range*(kit*350+head*50+grud*50+monopod*50+poplavok*50+truba*50+noga*50+shlem*50+kayt*50+foto*50+kepka*50+car*50+pult*50+akk*50+sd*50);
+        sum_15 = range*(kit*300+head*50+grud*50+monopod*50+poplavok*50+truba*50+noga*50+shlem*50+kayt*50+foto*50+kepka*50+car*50+pult*50+akk*50+sd*50);
+        sum_30 = range*(kit*250+head*50+grud*50+monopod*50+poplavok*50+truba*50+noga*50+shlem*50+kayt*50+foto*50+kepka*50+car*50+pult*50+akk*50+sd*50);
+        sum_50 = range*(kit*180+head*50+grud*50+monopod*50+poplavok*50+truba*50+noga*50+shlem*50+kayt*50+foto*50+kepka*50+car*50+pult*50+akk*50+sd*50);
+
+        days.html(range);
+
+        if (range <= 3) {
+            summa.html(sum_0);
+            skidka.html((sum_0 - summa.html()));
+            if (range == 0) {
+                sum_per_day.html(0);
+            }
+            else {
+                sum_per_day.html(summa.html()/range);
+            }
+        }
+        else if ((range >= 4) && (range <= 9)) {
+            summa.html(sum_15);
+            skidka.html((sum_0 - summa.html()));
+            sum_per_day.html(summa.html()/range);
+        }
+        else if ((range >= 10) && (range <= 19)) {
+            summa.html(sum_30);
+            skidka.html((sum_0 - summa.html()));
+            sum_per_day.html(summa.html()/range);
+        }
+        else if (range >= 20) {
+            summa.html(sum_50);
+            skidka.html((sum_0 - summa.html()));
+            sum_per_day.html(summa.html()/range);
+        }
     }, 200);
-    var currentNumber = $(".range").val();
-    var t;
 
 
-    $({numberValue: currentNumber}).animate({numberValue: 25}, {
-        duration: 2000,
-        easing: 'linear',
-        step: function() {
-            $(".range").val(Math.ceil(this.numberValue));
-            t=$(".range").val();
+    //increment inputs
+
+    $(".minus").on("click", function() {
+        var input, val;
+        input = $(this).next();
+
+        if (input.val() != 0) {
+            val = parseInt(input.val());
+            val--;
+            input.val(val);
         }
     });
-    setTimeout(function() {
-        $({numberValue: t}).animate({numberValue: 14}, {
-            duration: 500,
-            easing: 'linear',
-            step: function() {
-                $(".range").val(Math.ceil(this.numberValue));
-            }
-        });
-    }, 2000);
+    $(".plus").on("click", function() {
+        var input, val;
+        input = $(this).prev();
+        val = parseInt(input.val());
+        val++;
+        input.val(val);
+    });
+
+    //onselect disable
+
+    $("td:first-child").attr({
+        onselectstart: "return false",
+        onmousedown: "return false"
+    });
+
 
 });
